7ac090139b3862266eb8488fecec5cc74f3849f1
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2024-12-08 10:56:40 +0700

    feat: initial layout

--- src/app/workshop/dashboard/page.tsx
+++ src/app/workshop/dashboard/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
 import { menuGroups } from "@/app/dashboard/_data/menu";
+import { triggerEdgeCollapse, triggerEdgeDrawer } from "tailwindcss-jun-layout";
 
 export default function WorkshopDashboard() {
-  return <div>Edit me...</div>;
+  return (
+    <div className="jun-layout">
+      <header className="jun-header">
+        <button className="jun-edgeDrawerTrigger" onClick={() => triggerEdgeDrawer()}>
+          Toggle
+        </button>
+        <button
+          className="jun-edgeCollapseTrigger"
+          onClick={(event) => triggerEdgeCollapse({ event })}
+        >
+          Collapse
+        </button>
+        Header
+      </header>
+      <aside
+        className="
+        jun-edgeSidebar
+        jun-edgeSidebar-w-[200px]
+        jun-edgeSidebar-drawer
+        sm:jun-edgeSidebar-permanent
+        sm:jun-edgeSidebar-collapsed-w-[48px]
+        sm:jun-edgeSidebar-permanent-hoverUncollapse
+        jun-edgeSidebar-permanent-autoCollapse-lg
+        "
+      >
+        <div className="jun-edgeContent">Sidebar content</div>
+      </aside>
+      <main className="jun-content">Content</main>
+      <footer className="jun-footer">Footer</footer>
+    </div>
+  );
 }
